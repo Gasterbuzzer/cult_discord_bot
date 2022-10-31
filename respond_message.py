cd72afb37fa340a649f4bee1cd607121d62253c9
Gasterbuzzer <juliankoberg@hotmail.com>
2022-10-31 10:39:00 +0100

    Help Updated

--- respond_message.py
+++ respond_message.py
@@ -251,6 +251,11 @@ def handle_message(m, all_m, all_m_without_, client_r, message_object, author):
         embed.add_field(name=f"{prefix}ritual", value="Start a ritual in your current voice channel!",
                         inline=False)
 
+        embed.add_field(name=chr(173), value=chr(173))
+
+        embed.add_field(name=f"{prefix}settings", value="Opens up the settings menu for the bot.",
+                        inline=False)
+
         embed.set_thumbnail(
             url="attachment://image.png")  # Use the attachment url inorder to use local files and/or images
 
